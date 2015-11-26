ded0c87fcbf86ac30a39dd8d31edbab0002b911b
eom-eom <elbertmolina_25@hotmail.com>
2015-11-26 08:46:38 +0800

    log in

--- JobsOrderSystem/Forms/frmLogin.vb
+++ JobsOrderSystem/Forms/frmLogin.vb
@@ -1,6 +1,10 @@
 ﻿Imports System.Data.SqlClient
 Public Class frmLogin
-    'Dim db As New DBHelper(My.Settings.ConnectionString)
+    Dim db As New DBHelper(My.Settings.DBconn)
+    Dim dr As SqlClient.SqlDataReader
+    'Dim rec As Integer
+    'Dim data As New Dictionary(Of String, Object)
+
     'Dim dr As SQLite.SQLiteDataReader
     'Dim cmd As SQLite.SQLiteCommand
     Private Sub btnLog_Click(sender As Object, e As EventArgs) Handles btnLog.Click
@@ -10,11 +14,14 @@ Public Class frmLogin
         'dr = db.ExecuteReader("SELECT * FROM tbl_users")
 
         'Do While dr.Read
-        '    MsgBox(dr.Item(0).ToString)
-
+        '    MsgBox(dr.Item("user_name").ToString)
         'Loop
-        'Exit Sub
+
         Me.Hide()
         frmMain.Show()
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
